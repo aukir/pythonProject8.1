780359eea38100becfb1d43b8782af925b23ecf6
au-27 <159150805+au-27@users.noreply.github.com>
2024-02-24 23:06:03 +0600

    Initial commit

---
+++ FDataBase.py
@@ -0,0 +1,66 @@
+import time
+import sqlite3
+import math
+
+
+class FDataBase:
+
+    def __init__(self, db):
+        self.__db = db
+        self.__cur = db.cursor()
+
+    def getMenu(self):
+        sql = '''SELECT * FROM mainmenu'''
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res:
+                return res
+        except:
+            print("ошибка")
+        return []
+
+    def addUser(self, name, email, hpsw):
+        try:
+            self.__cur.execute(f"SELECT COUNT() as 'count' FROM user WHERE email LIKE '{email}'")
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("пользователь с таким email уже существует")
+                return False
+
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO user VALUES(NULL, ?,?,?,?)", (name, email, hpsw, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("ошибка добавления пользователя в бд" + str(e))
+            return False
+
+        return True
+
+    def getUser(self, user_id):
+        try:
+            self.__cur.execute(f"SELECT * FROM user WHERE id = {user_id} LIMIT 1")
+            res = self.__cur.fetchone()
+            if not res:
+                print("пользователь не найден")
+                return False
+            return res
+        except sqlite3.Error as e:
+            print("ошибка получения данных из бд" + str(e))
+        return False
+
+    def getUserByEmail(self, email):
+        try:
+            self.__cur.execute(f"SELECT * FROM user WHERE email = '{email}' LIMIT 1")
+            res = self.__cur.fetchone()
+            if not res:
+                print("пользователь не найден")
+                return False
+            return res
+        except sqlite3.Error as e:
+            print("ошибка получения данных из бд" + str(e))
+
+        return False
+
+
+
---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, render_template, request, session, redirect, url_for, abort, g, flash
+import sqlite3
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from FDataBase import FDataBase
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from UserLogin import UserLogin
+
+DATABASE = '/tmp/flsite.db'
+SECRET_KEY = 'roeu8u0qwo3w04h65y7o2iq'
+DEBUG = True
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "авторизуйся для доступа к странице "
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    print("load_user")
+    return UserLogin().fromDB(user_id, dbase)
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+@app.errorhandler(404)
+def f(error):
+    return render_template("vybor.html")
+
+
+@app.route('/', methods=['POST', 'GET'])
+def about():
+    db = get_db()
+    return render_template("vybor.html")
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+dbase = None
+@app.before_request
+def before_request():
+    global dbase
+    db = get_db()
+    dbase = FDataBase(db)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    db = get_db()
+    dbase = FDataBase(db)
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+
+    if request.method == "POST":
+        user = dbase.getUserByEmail(request.form.get('email'))
+        if user and check_password_hash(user['psw'], request.form['psw']):
+            userlogin = UserLogin().create(user)
+            rm = True if request.form.get('remainme') else False
+
+            login_user(userlogin, remember=rm)
+            session['userLogged'] = request.form['email']
+            return redirect(request.args.get("next") or url_for('profile'))
+
+    return render_template("about.html")
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    db = get_db()
+    dbase = FDataBase(db)
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['email']) > 4 and len(request.form['psw']) > 4:
+            hpsw = generate_password_hash(request.form['psw'])
+            res = dbase.addUser((request.form['name']), request.form['email'], hpsw)
+            if res:
+                flash("успешная регистрация")
+                return redirect(url_for('login'))
+            else:
+                flash("ошибка")
+    return render_template("register.html")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("вы вышли из аккаунта", "success")
+    return redirect(url_for('login'))
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return f"""<p><a href="{url_for('logout')}">выйти из профиля</a> 
+    <p>user info: {current_user.get_id()}"""
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
